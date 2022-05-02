3ad38ff68d00457f59b792eb8d7c521e17be4a25
Azure152 <92547768+Azure152@users.noreply.github.com>
2022-05-02 14:46:32 -0500

    Add files via upload

---
+++ corvusuni.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-05-2022 a las 21:44:12
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `corvusuni`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `homework`
+--
+
+CREATE TABLE `homework` (
+  `id` int(11) DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `description` text COLLATE utf8_spanish_ci DEFAULT NULL,
+  `datelimit` date DEFAULT NULL,
+  `timelimit` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
